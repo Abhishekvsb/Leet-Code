a023093162555766779b0d71c0017bef74503de9
Abhishek <abhishekkckv@gmail.com>
2026-09-24 12:12:00 +0530

    48 MB (5.76%)

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.toLowerCase();
+        String b="";
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(Character.isLetterOrDigit(ch))
+            {
+                b=b+ch;
+            }
+        }
+        String c="";
+        for(int i=b.length()-1;i>=0;i--)
+        {
+            c=c+b.charAt(i);
+        }
+        return b.equals(c);
+    }
+}
